12dbfd52057bd3cc3e6d1859cfa7ed728a68902a
wiktormuller <biznes.wiktor@gmail.com>
2020-02-09 12:19:04 +0100

    Jagged Array

---
+++ AlgorithmsAndDataStructures/DataStructures/TransportEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndDataStructures.DataStructures
+{
+    public enum TransportEnum
+    {
+        CAR,
+        BUS,
+        SUBWAY,
+        BIKE,
+        WALK
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/TransportEnumExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndDataStructures.DataStructures
+{
+    public static class TransportEnumExtensions
+    {
+        public static char GetChar(this TransportEnum transport)
+        {
+            switch(transport)
+            {
+                case TransportEnum.BIKE:
+                    return 'R';
+                case TransportEnum.BUS:
+                    return 'A';
+                case TransportEnum.CAR:
+                    return 'S';
+                case TransportEnum.SUBWAY:
+                    return 'M';
+                case TransportEnum.WALK:
+                    return 'P';
+                default: throw new Exception("Nieznany środek transportu.");
+            }
+        }
+        public static ConsoleColor GetColor(this TransportEnum transport)
+        {
+            switch(transport)
+            {
+                case TransportEnum.BIKE:
+                    return ConsoleColor.Blue;
+                case TransportEnum.BUS:
+                    return ConsoleColor.DarkGreen;
+                case TransportEnum.CAR:
+                    return ConsoleColor.Red;
+                case TransportEnum.SUBWAY:
+                    return ConsoleColor.DarkMagenta;
+                case TransportEnum.WALK:
+                    return ConsoleColor.DarkYellow;
+                default:
+                    throw new Exception("Nieznany środek transportu.");
+            }
+        }
+    }
+}
