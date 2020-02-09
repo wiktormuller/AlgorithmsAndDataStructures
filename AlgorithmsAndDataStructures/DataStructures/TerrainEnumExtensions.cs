a0080bbc82073888ae67ecbf8e45277a8d857fa0
wiktormuller <biznes.wiktor@gmail.com>
2020-02-09 11:10:33 +0100

    Multi Dimendion Array

---
+++ AlgorithmsAndDataStructures/DataStructures/TerrainEnumExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndDataStructures.DataStructures
+{
+    public static class TerrainEnumExtensions   //EXTENSION CLASSES AND METHODS HAVE TO BE STATIC
+    {
+        public static ConsoleColor GetColor(this TerrainEnum terrain)
+        {
+            switch(terrain)
+            {
+                case TerrainEnum.GRASS:
+                    return ConsoleColor.Green;
+                case TerrainEnum.SAND:
+                    return ConsoleColor.Yellow;
+                case TerrainEnum.WALL:
+                    return ConsoleColor.Blue;
+                default:
+                    return ConsoleColor.DarkGray;
+            }
+        }
+
+        public static char GetChar(this TerrainEnum terrain)
+        {
+            switch(terrain)
+            {
+                case TerrainEnum.GRASS:
+                    return '\u201c';
+                case TerrainEnum.SAND:
+                    return '\u25cb';
+                case TerrainEnum.WALL:
+                    return '\u2248';
+                default:
+                    return '\u25cf';
+            }
+        }
+    }
+}
--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AlgorithmsAndDataStructures.DataStructures;
+using System;
 using System.Globalization;
+using System.Text;
 
 namespace AlgorithmsAndDataStructures
 {
@@ -8,11 +10,12 @@ namespace AlgorithmsAndDataStructures
         static void Main(string[] args)
         {
             oneDimensionArray();
+            multiDimensionArray();
 
             Console.ReadKey();
         }
 
-        public static void oneDimensionArray()
+        public static void oneDimensionArray()  //NAME OF DAYS IN ACTUAL MONTH
         {
             string[] days = new string[7];
 
@@ -26,6 +29,47 @@ namespace AlgorithmsAndDataStructures
             {
                 Console.WriteLine($"-> {day}");
             }
+            Console.WriteLine();
+        }
+
+        public static void multiDimensionArray()   //MULTIPLICATION TABLE
+        {
+            int[,] results = new int[10, 10]; //[rows, columns]
+
+            for(int i = 0; i < results.GetLength(0); i++)   //GetLength(dimension)
+            {
+                for(int j = 0; j < results.GetLength(1); j++)
+                {
+                    results[i, j] = (i + 1) * (j + 1);
+                }
+            }
+            for (int i = 0; i < results.GetLength(0); i++)
+            {
+                for (int j = 0; j < results.GetLength(1); j++)
+                {
+                    Console.Write("{0,4}", results[i,j]);   //DISPLAY EVERY CHAR WITH 4 CHARS IN CONSOLE
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            TerrainEnum[,] map =
+            {
+                { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+                { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.WATER }
+            };
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            
+            for(int row = 0; row < map.GetLength(0); row++)
+            {
+                for(int column = 0; column < map.GetLength(1); column++)
+                {
+                    Console.ForegroundColor = map[row, column].GetColor();
+                    Console.Write(map[row, column].GetChar() + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
